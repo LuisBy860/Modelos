24e7fcec9d8b9c3f0881d858ca4a1d09065bf638
Luis Serrano <84140881+LuisBy860@users.noreply.github.com>
2021-09-12 17:30:23 -0600

    Agregar archivos de proyecto.

---
+++ Modelos/Models/ViewModels/PersonaViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Modelos.Models.ViewModels
+{
+    public class PersonaViewModel
+    {
+        [Display (Name = "Monto")]
+        
+        [Required(ErrorMessage ="Debe rellenar los campos")]
+        public int Monto { get; set; }
+        
+
+        [Display(Name = "Nombre")]
+
+        [Required(ErrorMessage = "Debe rellenar los campos")]
+        public string Nombre { get; set; }
+    }
+}
